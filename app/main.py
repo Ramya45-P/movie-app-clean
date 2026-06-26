47423e87762ee127527237e6b708261db758daff
Ramya <pramya9912@gmail.com>
2026-06-26 09:34:05 +0530

    Fix Vercel CORS URL

--- app/main.py
+++ app/main.py
@@ -30,10 +30,10 @@ app.add_middleware(
     "http://localhost:3000",
     "http://localhost:3001",
     "https://movie-app-clean-lkmwomqbq-ramya8.vercel.app",
-]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+   ]
 )
 
 
