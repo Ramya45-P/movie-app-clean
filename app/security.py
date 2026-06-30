417311ccec796366b69e6f0d978ce319c8a07b87
Ramya <pramya9912@gmail.com>
2026-06-30 14:47:23 +0530

    fix security import error

--- app/security.py
+++ app/security.py
@@ -1,9 +1,9 @@
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
-from jose import JWTError, jwt
+from jose import jwt
 
 # -------------------------
-# PASSWORD HASHING
+# CONFIG
 # -------------------------
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -12,11 +12,14 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_DAYS = 7
 
 
+# -------------------------
+# PASSWORD FUNCTIONS
+# -------------------------
 def get_password_hash(password: str):
     return pwd_context.hash(password)
 
 
-def verify_password(plain_password, hashed_password):
+def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
 
 
@@ -33,6 +36,4 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
 
     to_encode.update({"exp": expire})
 
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
-    return encoded_jwt
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
