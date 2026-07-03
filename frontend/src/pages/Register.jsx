dec78fff44e04e0ca57b6b9401d5005738d20ab7
Ramya <pramya9912@gmail.com>
2026-07-03 11:54:38 +0530

    Implement Register page functionality

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import API from "../api/axios";
+import { useNavigate } from "react-router-dom";
+
+function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      await API.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+
+      alert("Registration successful!");
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
+      alert("Registration failed");
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Register</h2>
+
+      <form onSubmit={handleRegister}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
